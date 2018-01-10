727366cd950ebad6f406432a82827eeb19cd6e98
chhavi02 <chhavijain2202@gmail.com>
2018-01-10 10:30:23 +0530

    second commit

---
+++ public/javascripts/index.js
@@ -0,0 +1,11 @@
+/* Set the width of the side navigation to 250px */
+	function openNav() {
+	    document.getElementById("mySidenav").style.width = "200px";
+	    document.body.style.backgroundColor = "#CFD8DC";
+	}
+
+	/* Set the width of the side navigation to 0 */
+	function closeNav() {
+	    document.getElementById("mySidenav").style.width = "0";
+	    document.body.style.backgroundColor = "white";
+	}
