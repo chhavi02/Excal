84a0a0577a44dc5d5f02a556fa9bc9823140d41a
sethipawandeep <pawandeep2007@gmail.com>
2018-01-17 17:20:53 +0530

    Controllers

--- controllers/login.js
+++ controllers/login.js
@@ -1,5 +1,23 @@
 var bcrypt = require('bcrypt');
 
 exports.loginPOST = function(req, res) {
-	
+	console.log('Login POST Method');
+	console.log(req.body);	
+	// Select from employee table where empid = req.body.empId
+	// Promise
+	.then(function(data) {
+		bcrypt.compare(req.body.password, data.password, function(error, passMatch) {
+			if(error) {
+				console.log('Password validation failed.');
+			/*	res.json({
+					responseMessage: 'Failed authentication'
+				});*/
+				res.render('login');
+			}
+			if(passMatch === true) {
+				
+			}
+
+		})
+	}, function(error));
 }
