99a4c87f437f4d44bea3c08c1af354bc3941e4f5
chhavi02 <chhavijain2202@gmail.com>
2018-01-17 19:03:10 +0530

    location done

--- public/javascripts/createCentre.js
+++ public/javascripts/createCentre.js
@@ -1,7 +1,7 @@
-var map;
+
 function initMap() {
 	var uluru = {lat: -34.397, lng: 150.644};
-	map = new google.maps.Map(document.getElementById('map'), {
+	var map = new google.maps.Map(document.getElementById('map'), {
 	zoom: 10,
 	center: uluru
    });
@@ -11,3 +11,33 @@ var marker = new google.maps.Marker({
     map: map
    });
 	}
+$('#findCentre').on('click', function(e){
+	var centre = document.getElementById("centre").value;
+	console.log(e);
+	$.ajax({
+		url: "https://maps.googleapis.com/maps/api/geocode/json?address=" + centre + "&key= AIzaSyDW7MjMNbjibsp-tLFd9g9Ol-xc9NuIge4",
+		type: "GET",
+		dataType: "json",
+		success: function( response ){
+			console.log(response);
+
+			var lati = response.results[0].geometry.location.lat;
+		          	var long = response.results[0].geometry.location.lng;
+		          	console.log('Lat: ' + lati);
+		          	console.log('Long: ' + long);
+		          	var location= {lat: lati, lng: long};
+		          	var map = new google.maps.Map(document.getElementById('map'), {
+			          zoom: 12,
+			          center: location
+			        });
+			          var marker = new google.maps.Marker({
+			        	position: location,
+			        	map: map
+			        });
+			},
+		error: function(error){
+			console.log('the view duties page was not loaded', error);
+		}
+	});
+
+})
