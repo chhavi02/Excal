f4fb2c74aa7f0f0edf5d4427541bb0892c2fa3e0
sethipawandeep <pawandeep2007@gmail.com>
2018-01-17 18:17:36 +0530

    Current state

--- controllers/login.js
+++ controllers/login.js
@@ -5,19 +5,19 @@ exports.loginPOST = function(req, res) {
 	console.log(req.body);	
 	// Select from employee table where empid = req.body.empId
 	// Promise
-	.then(function(data) {
+	/*.then(function(data) {
 		bcrypt.compare(req.body.password, data.password, function(error, passMatch) {
 			if(error) {
 				console.log('Password validation failed.');
 			/*	res.json({
 					responseMessage: 'Failed authentication'
 				});*/
-				res.render('login');
+	/*			res.render('login');
 			}
 			if(passMatch === true) {
 				
 			}
 
 		})
-	}, function(error));
+	}, function(error));*/
 }
