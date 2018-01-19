12d855958025a49881e2328653b68548a42cf458
sethipawandeep <pawandeep2007@gmail.com>
2018-01-19 23:16:56 +0530

    Attendance route. Not working as of now.

--- routes/attendance.js
+++ routes/attendance.js
@@ -2,8 +2,13 @@ var express = require('express');
 var router = express.Router();
 
 router.get('/', function(req, res) {
-	console.log(req);
+	// console.log(req.body);
 	res.render('attendance');
 });
+
+router.post('/', function(req, res) {
+	console.log(req.body);
+	res.redirect('/');
+})
 
 module.exports = router;
